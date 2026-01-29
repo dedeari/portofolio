a719a5a5fe76bd7190919b965b5144bf0f545cc4
Dede Ari <dedeari006@gmail.com>
2026-01-30 01:05:28 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['index.html'],
-  darkMode: 'class',
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+  ],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary:'#14b8a6',
-        dark:'#0f172a',
-        secondary:'#64748b',
+        primary: "#14b8a6",
+        dark: "#0f172a",
+        secondary: "#64748b",
       },
       screens: {
-        '2xl': '1320px',
-      }
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
-
+};
